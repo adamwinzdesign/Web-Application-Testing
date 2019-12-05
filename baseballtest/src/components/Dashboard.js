6d889be05ad1fca14f3216d9e50a3161afe3ef07
adamwinzdesign <adamwinzdesign@gmail.com>
2019-12-05 16:03:04 -0600

    added basic functionality

--- baseballtest/src/components/Dashboard.js
+++ baseballtest/src/components/Dashboard.js
@@ -1,5 +1,63 @@
 import React from 'react';
+import Display from './Display';
 
 export const Dashboard = () => {
-  
+  const [balls, setBalls] = useState(0);
+  console.log(`balls: ${balls}`);
+  const [strikes, setStrikes] = useState(0);
+  console.log(`strikes: ${strikes}`);
+  const [outs, setOuts] = useState(0);
+  console.log(`outs: ${outs}`);
+
+  const addOuts = () => {
+    if(outs === 2) {
+      setOuts(0);
+      console.log('Outs reset!')
+    } else {
+      setOuts(outs => outs + 1)
+    }
+  }
+
+  const addBalls = () => {
+    if(balls === 3) {
+      resetCount();
+      console.log(`balls reset!`)
+    } else {
+      setBalls(balls => balls + 1);
+    }
+  }
+
+  const addStrikes = () => {
+    if(strikes === 2) {
+      resetCount();
+      addOuts();
+      console.log('strikes reset!')
+    } else {
+      setStrikes(strikes => strikes + 1);
+    }
+  }
+
+  const foulBall = () => {
+    if(strikes === 2) {
+      return;
+    } else {
+      setStrikes(strikes => strikes + 1);
+    }
+  }
+
+  const resetCount = () => {
+    setStrikes(0);
+    setBalls(0);
+  }
+
+  return(
+    <div>
+      <h1>Dashboard</h1>
+      <button onClick={foulBall}>Foul Ball!</button>
+      <button onClick={addStrikes}>Strike!</button>
+      <button onClick={addBalls}>Ball!</button>
+      <button onClick={resetCount}>End of at-bat!</button>
+      <Display strikes={strikes} balls={balls} outs={outs}/>
+    </div>
+  )
 }
--- baseballtest/src/components/Display.js
+++ baseballtest/src/components/Display.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-export const Display = () => {
-  
+export const Display = (props) => {
+  console.log(props);
+
+  return(
+    <div>
+      <h1>Game Display:</h1>
+      <h2>Current Strikes: {props.strikes}</h2>
+      <h2>Current Balls: {props.balls}</h2>
+      <h2>Current Outs: {props.outs}</h2>
+    </div>
+  )
 }
